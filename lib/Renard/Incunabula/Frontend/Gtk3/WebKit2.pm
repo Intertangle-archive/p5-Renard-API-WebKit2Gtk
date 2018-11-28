d6a270fe4d0824174cbffdb704e12a3367c9bb9f
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-11-27 23:21:49 -0600

    Add wiki repo link

--- lib/Renard/Incunabula/Frontend/Gtk3/WebKit2.pm
+++ lib/Renard/Incunabula/Frontend/Gtk3/WebKit2.pm
@@ -4,3 +4,8 @@ use strict;
 use warnings;
 
 1;
+=head1 SEE ALSO
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Renard-Incunabula-Frontend-Gtk3-WebKit2/>
+
+=cut
